7928ddf52668fa3d6e54b83f7b9157eab6016e96
alester77 <113203488+alester77@users.noreply.github.com>
2023-07-18 11:46:31 -0600

    added timer interval, display function, started check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,16 +25,60 @@ var timerDisplay = document.getElementById("timerDisplay");
 var scoreForm = document.getElementById("scoreForm");
 var initialsInput = document.getElementById("initials");
 
+var CurrentQuestionIndex = 0; //keep track of current question
+var score = 0; //keep track of score
+
 var currentQuestionIndex = 0;
 var timer;
 var timeRemaining = 60;
 
-// Function to start the quiz
-function startQuiz() {
-  startButton.style.display = "none";
-  timer = setInterval(updateTimer, 1000);
-  showQuestion();
+// function to start timer
+function Countdown (){
+  var timeRemaining = 60;
+  var timer = setInterval(function (){
+    timeRemaining--;
+    timerDisplay.textContent = timeRemaining;
+
+    if(timeRemaining === 0){
+      clearInterval(timer);
+      timerDisplay.textContent = "0";
+      displayScore();
+    }
+  }, 1000)
 }
 
+function displayQuestion(){
+  var currentQuestion = quizData[currentQuestionIndex];
+  questionArea.textContent = currentQuestion.question; //display question
+  optionsArea.innerHTML = ""; //clear option area
 
+  // display options
+  for (var i = 0; i < currentQuestion.options.length; i++) {
+    var optionLabel = document.createElement("label");
+    var optionInput = document.createElement("input");
+    optionInput.type = "radio";
+    optionInput.name = "option";
+    optionInput.value = currentQuestion.options[i];
+    optionLabel.appendChild(optionInput);
+    optionLabel.append(" " +currentQuestion.options[i]);
+    optionsArea.appendChild(optionLabel);
+  }
+}
+// check the selected answer
+function checkAnswer(){
+  var selectedOption = document.querySelector('input[name="option"]:checked');
+
+}
+
+// function to display quiz once score is done
+function displayScore (){
+
+}
+
+Countdown();
+
+// makes start quiz begin quiz
+startButton.addEventListener("click", function(){
+  displayQuestion();
+});
 
