820e1dd000c7a292f57e1a06d3501a17508db9be
alester77 <113203488+alester77@users.noreply.github.com>
2023-07-21 14:02:40 -0600

    working on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,63 @@
 // Quiz questions and answers
-const quizData = [
+var questions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
-    options: ["<javascript>", "<scripting>", "<js>", "<script>"],
-    correctAnswer: "<script>"
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<scripting>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<script>", correct: true },
+    ],
   },
   {
     question: "How do I add a comment inside of JS?",
-    options: ["Just type.", "//This is a comment", "`This is a comment", "!This is a comment"],
-    correctAnswer: "//This is a comment"
+    answers: [
+      { text: "Just type.", correct: false },
+      { text: "//This is a comment", correct: true },
+      { text: "`This is a comment", correct: false },
+      { text: "!This is a comment", correct: false },
+    ],
   },
   {
     question: "Which operator is used to assign a value to a variable?",
-    options: ["x", "=", "-", "*"],
-    correctAnswer: "="
+    answers: [
+      { text: "x", correct: false },
+      { text: "=", correct: true },
+      { text: "-", correct: false },
+      { text: "*", correct: false },
+    ],
+    correctAnswer: "=",
   },
 ];
 
-// Variables
-var startButton = document.getElementById("startButton");
-var questionArea = document.getElementById("questionArea");
-var optionsArea = document.getElementById("optionsArea");
-var timerDisplay = document.getElementById("timerDisplay");
-var scoreForm = document.getElementById("scoreForm");
-var initialsInput = document.getElementById("initials");
-
-var CurrentQuestionIndex = 0; //keep track of current question
-var score = 0; //keep track of score
+var questionElement = document.getElementById("question");
+var answerButtons = document.getElementById("answer-buttons");
+var nextButton = document.getElementById("next-btn");
 
 var currentQuestionIndex = 0;
-var timer;
-var timeRemaining = 60;
+var score = 0;
 
-// function to start timer
-function Countdown (){
-  var timeRemaining = 60;
-  var timer = setInterval(function (){
-    timeRemaining--;
-    timerDisplay.textContent = timeRemaining;
-
-    if(timeRemaining === 0){
-      clearInterval(timer);
-      timerDisplay.textContent = "0";
-      displayScore();
-    }
-  }, 1000)
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
 }
 
-function displayQuestion(){
-  var currentQuestion = quizData[currentQuestionIndex];
-  questionArea.textContent = currentQuestion.question; //display question
-  optionsArea.innerHTML = ""; //clear option area
+function showQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  var questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-  // display options
-  for (var i = 0; i < currentQuestion.options.length; i++) {
-    var optionLabel = document.createElement("label");
-    var optionInput = document.createElement("input");
-    optionInput.type = "radio";
-    optionInput.name = "option";
-    optionInput.value = currentQuestion.options[i];
-    optionLabel.appendChild(optionInput);
-    optionLabel.append(" " +currentQuestion.options[i]);
-    optionsArea.appendChild(optionLabel);
-  }
-}
-// check the selected answer
-function checkAnswer(){
-  var selectedOption = document.querySelector('input[name="option"]:checked');
+  // answerButtons.innerHTML = ""; //clear answer buttons before adding new
 
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    console.log(answer.text);
+    button.innerHTML = "hello" + answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+  });
 }
 
-// function to display quiz once score is done
-function displayScore (){
-
-}
-
-Countdown();
-
-// makes start quiz begin quiz
-startButton.addEventListener("click", function(){
-  displayQuestion();
-});
-
+startQuiz();
